b3081a3f4b62c6cf339121c47f38f5ee531186fb
trunglebka <16150250+trunglebka@users.noreply.github.com>
2019-02-15 08:35:08 +0700

    Update rest-tts.py

--- python/tts/rest/rest-tts.py
+++ python/tts/rest/rest-tts.py
@@ -7,9 +7,11 @@ import os.path
 from pathlib import Path
 
 url = "https://vtcc.ai/voice/api/tts/v1/rest/syn"
-data = {"text": "Đây là chương trình demo tổng hợp tiếng nói của trung tâm không gian mạng", "voice": "doanngoclev2", "id": "2", "without_filter": False, "speed": 1.0,
+#edit text field as your need
+data = {"text": "Đây là chương trình demo tổng hợp tiếng nói của trung tâm không gian mạng", "voice": "doanngocle", "id": "2", "without_filter": False, "speed": 1.0,
         "tts_return_option": 2, "timeout": 60000, "addition_params": [{ "key": "", "value": ""}]}
-headers = {'Content-type': 'application/json', 'token': 'anonymous'}
+#add your token to authenticate
+headers = {'Content-type': 'application/json', 'token': ''}
 s = requests.Session()
 dirname = os.path.dirname(__file__)
 audio_path = os.path.join(Path(dirname).parent, 'test_audio.wav')
@@ -25,5 +27,6 @@ print(response.status_code)
 
 # Get the response data as a python object.
 data = response.content
+# generated wav file is ./python-example.wav
 with open("python-example.wav", "wb") as f:
         f.write(data)
